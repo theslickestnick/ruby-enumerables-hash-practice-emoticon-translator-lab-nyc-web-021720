fe4b97ff1927ef460f03c275c22ec8b02ef33cd0
Nicholas Echevarria <nick.echev@gmail.com>
2020-02-13 20:45:05 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@ require "pry"
 def load_library(yml_file)
   require 'yml_file'
   # japanese_emoticons = {:get_meaning => {}, :get_emoticon => {}}
-  japanese_emoticon = yml_file.load_file('yml_file')
+  japanese_emoticon = YAML.load_file('yml_file')
 end
 
 def get_japanese_emoticon
