b988c7c73195f733b6c62dbb1847baa30ab6a35c
Nicholas Echevarria <nick.echev@gmail.com>
2020-02-13 22:43:54 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,8 +5,8 @@ def load_library(yml_file)
   result = {:get_meaning => {}, :get_emoticon => {}}
   emoticon_set = YAML.load_file(yml_file)
   emoticon_set.each do |emotion, associated_emoticon|
-      binding.pry
     result[:get_meaning][associated_emoticon[1]] = emotion
+    binding.pry
       emoticon_set.each do |associated_emoticon|
         result[:get_emoticon] = emoticon_set[associated_emoticon]
     end
