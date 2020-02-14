5e2f5e06b4d56a665d72fbd74808c8da96bc7dc5
Nicholas Echevarria <nick.echev@gmail.com>
2020-02-14 02:28:40 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,9 +16,9 @@ def get_japanese_emoticon(yml_file, emoticon)
   total_set_of_emoticons = load_library(yml_file)
   total_set_of_emoticons[:get_emoticon].each do |western_emoticon, japanese_emoticon|
     emoticon == western_emoticon ? japanese_emoticon : nil
-  binding.pry
+  # binding.pry
   end
-  
+
 end
 
 def get_english_meaning
